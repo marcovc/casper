4cf611f575095929495a3c934a38a407e6a9022c
Marco Correia <marco.v.correia@gmail.com>
2011-11-05 00:37:12 +0000

    NEW: python bindings

--- examples/bindings/python/test.py
+++ examples/bindings/python/test.py
@@ -7,40 +7,41 @@ n = 4
 
 solver = Solver()
 
-#vars = IntVarArray(solver,n,1,n)
-#
-#solver.post(distinct(vars))
-#solver.post(distinct([vars[i]+i for i in range(0,n)]))
-#solver.post(distinct([vars[i]-i for i in range(0,n)]))
-#
-#found = solver.solve(label(solver,vars,selectVarMinDom(solver,vars),selectValsRand(solver,vars)));
-#
-#while found:
-#    print vars
-#    found = solver.next()
-#    
-#print solver.getStats()
-#print solver.getCPUTimer()
+vars = IntVarArray(solver,n,1,n)
 
-p = IntPar(solver.getState(),lambda:8)
-e = IntExpr(lambda:8)
-print e
-print "bla"
-#d = IntPar(lambda:8)
+solver.post(distinct(vars))
+solver.post(distinct([vars[i]+i for i in range(0,n)]))
+solver.post(distinct([vars[i]-i for i in range(0,n)]))
 
-#print f
-#print gg
+x = IntVar(solver)
+v = IntPar(solver)
+
+search = whiledo(lambda:not vars.ground()) (
+					assign(x,lambda:min(vars,key=domainSize)) &	
+					assign(v,lambda:min(x)) &
+					(post(solver,x==v) | post(solver,x!=v)) 
+				)
+
+found = solver.solve(search);
+
+if found:
+    print vars
+else:
+	print "no solution"
+	    
+print solver.getStats()
+print solver.getCPUTimer()
+
 #found = solver.solve(assign(p,2) | assign(p,lambda:8))
-
+#
 #while found:
 #    print p
 #    found = solver.next()
-
-#i << lambda:min(j for j in range(1,3)))
-#j << 
-#solver.post(distinct(aggregate(i,range(1,10)) (vars[i]+i)))
-
-
-
+#
+#
+#p = IntPar(solver.getState(),lambda:8)
+#e = IntExpr(lambda:8)
+#print e
+#print "bla"
         
              
