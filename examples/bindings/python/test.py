d12c4bdcb75907dad47d9e725ab60f2c27bee455
Marco Correia <marco.v.correia@gmail.com>
2011-11-25 16:28:46 +0000

    NEW: python bindings

--- examples/bindings/python/test.py
+++ examples/bindings/python/test.py
@@ -4,7 +4,7 @@ from casper import *
 from casper.cp import *
 import random
 
-n = 8
+n = 100
 
 solver = Solver()
 
@@ -14,27 +14,23 @@ solver.post(distinct(vars))
 solver.post(distinct([vars[i]+i for i in range(0,n)]))
 solver.post(distinct([vars[i]-i for i in range(0,n)]))
 
-#
-#x = IntVar(solver)
-#v = IntPar(solver.getState())
-#
-x = IntVarRef(solver)
-v = IntRef(solver)
-search = whiledo(lambda:not vars.ground()) (
-					assign(x,min(vars,key=lambda x: len(x.domain()))) &
-					assignMin(x,vars,domainSize(x)*ground(x)))	
-					assign(v,lambda:min(x)) &
-					(post(solver,x==v) | post(solver,x!=v)) 
-				)
+#x = IntVarRef(solver)
+#v = IntRef(solver)
+#search = whiledo(lambda:not vars.ground()) (
+#					assign(x,min(vars,key=lambda x: len(x.domain()))) &
+#					assignMin(x,vars,domainSize(x)*ground(x)))	
+#					assign(v,lambda:min(x)) &
+#					(post(solver,x==v) | post(solver,x!=v)) 
+#				)
 
 
 def search():
+	builtin = __builtins__ 
 	if vars.ground():
 		return True
 
-	idx = __builtins__.min(range(n),
-						key=lambda idx: vars[idx].ground()*n*n+len(vars[idx].domain())*n+
-								__builtins__.abs(idx-n/2))
+	idx = mmin(range(n),key=lambda idx: vars[idx].ground()*n*n+len(vars[idx].domain())*n+
+								builtin.abs(idx-n/2))
 	x = vars[idx]
 #	x = __builtins__.min(nonground,key=lambda v: len(v.domain()))
 	v = random.choice(list(x.domain()))
